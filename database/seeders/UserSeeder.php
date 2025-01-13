19a264f0424fb956f818dd6ab9b788e371014fc6
Jedd Saliba <jeddsaliba@gmail.com>
2025-01-13 14:01:47 +0800

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use BezhanSalleh\FilamentShield\Support\Utils;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +17,11 @@ class UserSeeder extends Seeder
     {
         User::factory()->count(15)->create()->each(function ($user) {
             $user->assignRole(Utils::getPanelUserRoleName());
+            $date = Carbon::now();
+            $user->profile()->create([
+                'phone' => fake()->unique()->e164PhoneNumber(),
+                'birthdate' => fake()->unique()->dateTimeBetween($date->copy()->subYears(75), $date->copy()->subYears(18))
+            ]);
         });
     }
 }
